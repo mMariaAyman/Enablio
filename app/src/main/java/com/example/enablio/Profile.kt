538ae930ac0c7e2a40f21617ff7cd6b8e98deedd
Maria <mera.ayman11@gmail.com>
2024-02-06 20:23:38 +0200

    profile

--- app/src/main/java/com/example/enablio/Profile.kt
+++ app/src/main/java/com/example/enablio/Profile.kt
@@ -2,10 +2,13 @@ package com.example.enablio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
     }
 }
