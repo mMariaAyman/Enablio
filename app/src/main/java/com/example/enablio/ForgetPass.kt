dc4249a55d4673e931cf9d87405fafb390b4ee7c
Maria <mera.ayman11@gmail.com>
2024-02-08 13:42:18 +0200

    forgetpass

--- app/src/main/java/com/example/enablio/ForgetPass.kt
+++ app/src/main/java/com/example/enablio/ForgetPass.kt
@@ -2,10 +2,14 @@ package com.example.enablio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.enablio.databinding.ActivityForgetPassBinding
 
 class ForgetPass : AppCompatActivity() {
+    private lateinit var binding:ActivityForgetPassBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forget_pass)
+        binding = ActivityForgetPassBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
