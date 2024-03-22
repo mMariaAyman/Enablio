02080c4c7a72660f2072f520d0d7a9675519f2b0
Maria <mera.ayman11@gmail.com>
2024-03-22 18:17:02 +0200

    profile

--- app/src/main/java/com/example/enablio/ProfileDis.kt
+++ app/src/main/java/com/example/enablio/ProfileDis.kt
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.enablio.databinding.ActivityProfileBinding
 import com.facebook.login.LoginManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class ProfileDis : AppCompatActivity() {
     private lateinit var binding:ActivityProfileBinding
@@ -20,13 +26,101 @@ class ProfileDis : AppCompatActivity() {
         setContentView(binding.root)
         setTitle("My Profile")
         auth = FirebaseAuth.getInstance()
-        binding.editNameTxt.text.append(auth.currentUser?.displayName.toString())
-        binding.userName.text = auth.currentUser?.displayName.toString()
+        rootFBRef = FirebaseDatabase.getInstance().getReference("Disabled")
+        rootFBRef.child(auth.currentUser?.uid.toString()).child("name").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+                binding.editNameTxt.text.append(value.toString())
+                binding.userName.text = value.toString()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        })
+        rootFBRef.child(auth.currentUser?.uid.toString()).child("gender").addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+                binding.gender.text.append(value.toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        })
         binding.editEmailTxt.text.append(auth.currentUser?.email.toString())
 
         binding.saveProfile.setOnClickListener {
-            binding.editNameTxt.text.toString()
+            val map = mapOf<String,String>(
+                "name" to binding.editNameTxt.text.toString(),
+                "gender" to binding.gender.text.toString(),
+
+                )
+            rootFBRef.child(auth.currentUser?.uid.toString()).updateChildren(map).addOnSuccessListener {
+                Toast.makeText(this,"Updated Successfully!", Toast.LENGTH_SHORT).show()
+                rootFBRef.child(auth.currentUser?.uid.toString()).child("name").addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val value = dataSnapshot.value
+                        binding.editNameTxt.text.clear()
+                        binding.editNameTxt.text.append(value.toString())
+                        binding.userName.text = value.toString()
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Handle error
+                    }
+                })
+                rootFBRef.child(auth.currentUser?.uid.toString()).child("gender").addListenerForSingleValueEvent(object :
+                    ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val value = dataSnapshot.value
+                        binding.gender.text.clear()
+                        binding.gender.text.append(value.toString())
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Handle error
+                    }
+                })
+
+            }
+
         }
+        binding.deleteProfile.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Confirm")
+                .setMessage("Are you sure to delete this Account?")
+                .setPositiveButton("Delete") { dialog, _ ->
+                    // Handle positive button click
+                    dialog.dismiss()
+                    rootFBRef.child(auth.currentUser?.uid.toString()).removeValue()
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "User deleted Successfully", Toast.LENGTH_SHORT).show()
+
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, "Failed to delete User", Toast.LENGTH_SHORT).show()
+
+                        }
+                    auth.currentUser?.delete()
+                        ?.addOnSuccessListener {
+                            Toast.makeText(this, "User deleted Successfully", Toast.LENGTH_SHORT).show()
+                        }
+                    val intent = Intent(this, LoginDis::class.java)
+                    startActivity(intent)
+                }
+                .setNegativeButton("Cancel") { dialog, _ ->
+                    // Handle negative button click
+                    dialog.dismiss()
+                }
+
+            val dialog = builder.create()
+            dialog.show()
+
+        }
+
 
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
